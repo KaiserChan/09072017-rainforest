358e2f5bd517db44e5d83bf84b84454709e78359
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-07 17:00:48 -0400

    added nested routes to Review model

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products
+  resources :products  do
+      resources :reviews, except: [:index, :new, :show]
+  end
+
   root 'products#index'
 
 end
