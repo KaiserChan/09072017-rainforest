3594943224161c0ceaf9e4ad2b2871e1e06c121e
KaiserChan <chankaiserkk@gmail.com>
2017-09-07 16:34:00 -0400

    updated rainforest - created review model and migration, updated product model and established association relationship

--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -1,10 +1,12 @@
 class Product < ApplicationRecord
 
+  has_many :reviews
+
   validates :name, :description, :price_in_cents, presence: true
   validates :price_in_cents, numericality: true
 
   def price_in_dollars
-    price_in_dollars = price_in_cents.to_f 
+    price_in_dollars = price_in_cents.to_f
     sprintf("$%.2f", price_in_dollars)
   end
 
