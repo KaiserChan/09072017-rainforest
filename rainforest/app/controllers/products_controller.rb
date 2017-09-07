1cd130fdb77011b804c21f7ffa59f25d9571bfd5
KaiserChan <chankaiserkk@gmail.com>
2017-09-07 10:56:32 -0400

    Updated rainforest - created products controller skeleton

---
+++ rainforest/app/controllers/products_controller.rb
@@ -0,0 +1,31 @@
+class ProductsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+end
