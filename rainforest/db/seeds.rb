9f6557e37102e50df363b9425514f440c942a2b6
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-07 16:54:19 -0400

    valided Review model and added seed data for Review

--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -8,5 +8,12 @@
 names=["Hug", "Pressure", "Mystery Box", "Uninvited Advice", "Nostril Hair Necklace"]
 descriptions=["You feel much better with this show of love", "Release! You live a healthy life without this but you can probably do with a dose of Hug", "Fulfills your desires and fantasies ... if you know what I mean ...", "Whether you like it or not, you are getting it!", "Makes you the most popular kid in the room!"]
 5.times do
-Product.create name:names.sample, description: descriptions.sample, price_in_cents:rand(1..100)
+Product.create! name:names.sample, description: descriptions.sample, price_in_cents:rand(1..100)
+end
+
+comments = ['Great!', 'I love it!','Improves my life!','Would recommed to a friend','Shutup and take my money!']
+products_id = [1,2,3,4,5]
+
+5.times do
+  Review.create! comment:comments.sample, product_id: products_id.sample
 end
