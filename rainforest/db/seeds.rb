913ec941f9fab0d010cafc2b53f385fd3f6f1ccc
KaiserChan <chankaiserkk@gmail.com>
2017-09-07 11:17:15 -0400

    Updated rainforest - created seed data

--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+names=["Hug", "Pressure", "Mystery Box", "Uninvited Advice", "Nostril Hair Necklace"]
+descriptions=["You feel much better with this show of love", "Release! You live a healthy life without this but you can probably do with a dose of Hug", "Fulfills your desires and fantasies ... if you know what I mean ...", "Whether you like it or not, you are getting it!", "Makes you the most popular kid in the room!"]
+5.times do
+Product.create name:names.sample, description: descriptions.sample, price_in_cents:rand(1..100)
+end
