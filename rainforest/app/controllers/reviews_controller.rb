b2fb2409023526fe97a651932423803879db82d3
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-07 18:08:52 -0400

    added Review form to Product details show veiw and created save feature to new comments

--- rainforest/app/controllers/reviews_controller.rb
+++ rainforest/app/controllers/reviews_controller.rb
@@ -1,6 +1,14 @@
 class ReviewsController < ApplicationController
 
   def create
+    @review = Review.new
+
+    @review.comment = params[:review][:comment]
+    @review.product_id = params[:product_id]
+
+    @review.save
+
+      redirect_to product_path(@review.product_id)
 
   end
 
